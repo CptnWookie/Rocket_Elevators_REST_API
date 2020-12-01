c9526cf48fa9825b79884d377e9bca9b23e33425
alex <alexandreleblanc89@gmail.com>
2020-12-01 13:40:12 -0500

    adding Customers Controller for Customer Portal queries

--- obj/Debug/netcoreapp3.1/RocketApi.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/RocketApi.AssemblyInfo.cs
@@ -11,7 +11,6 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("2ecf5b08-c5b4-497c-9af1-b3bcb01ee1e3")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("RocketApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
