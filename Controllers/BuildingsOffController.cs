eb6d14eee6ca71d3165dfd4162e309dfa2dfc291
alex <alexandreleblanc89@gmail.com>
2020-12-04 23:29:50 -0500

    update endpoint

--- Controllers/BuildingsOffController.cs
+++ Controllers/BuildingsOffController.cs
@@ -22,6 +22,23 @@ namespace RocketApi.Controllers
 
 
         // GET: api/buildings/customer/{id}
+        [HttpGet("Customers/{email}")]
+        public ActionResult<List<Buildings>> GetBuildingsFromCustomerEmail(long id)
+        {
+            List<Buildings> buildingsAll = _context.Buildings.ToList();
+            List<Buildings> buildingsFromCustomer = new List<Buildings>();
+            foreach (Buildings building in buildingsAll)
+            {
+                if (building.CustomerId == id)
+                {
+                    buildingsFromCustomer.Add(building);
+                }
+            }
+            return buildingsFromCustomer;
+        }
+
+
+        // GET: api/buildings/customer/{id}
         [HttpGet("Customers/{id}")]
         public ActionResult<List<Buildings>> GetBuildingsFromCustomer(long id)
         {
@@ -41,27 +58,24 @@ namespace RocketApi.Controllers
         // Retrieving a list of Buildings that contain at least one battery, column or elevator requiring intervention
         // GET: api/BuildingsOff
 
-        [HttpGet]
-        public IEnumerable<Buildings> GetBuildings()
-        {
+        // [HttpGet]
+        // public IEnumerable<Buildings> GetBuildings()
+        // {
+        //     IEnumerable<Buildings> Building =
 
+        //     from building in _context.Buildings
+        //     join battery in _context.Batteries on building.Id equals battery.BuildingId
+        //     join column in _context.Columns on battery.Id equals column.BatteryId
+        //     join elevator in _context.Elevators on column.Id equals elevator.ColumnId
+        //     where
+        //     battery.BatteryStatus == "Intervention" || battery.BatteryStatus == "Inactive"
+        //     || column.ColumnStatus == "Intervention" || column.ColumnStatus == "Inactive"
+        //     || elevator.ElevatorStatus == "Intervention" || elevator.ElevatorStatus == "Inactive"
+        //     select building;
 
+        //     return Building.Distinct().ToList();
 
-            IEnumerable<Buildings> Building =
-
-            from building in _context.Buildings
-            join battery in _context.Batteries on building.Id equals battery.BuildingId
-            join column in _context.Columns on battery.Id equals column.BatteryId
-            join elevator in _context.Elevators on column.Id equals elevator.ColumnId
-            where
-            battery.BatteryStatus == "Intervention" || battery.BatteryStatus == "Inactive"
-            || column.ColumnStatus == "Intervention" || column.ColumnStatus == "Inactive"
-            || elevator.ElevatorStatus == "Intervention" || elevator.ElevatorStatus == "Inactive"
-            select building;
-
-            return Building.Distinct().ToList();
-
-        }
+        // }
 
         private bool BuildingsExists(long id)
         {
