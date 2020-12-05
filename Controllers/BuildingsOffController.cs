3de9c07683c8729c6fbbeab3f050187e8f9b4a6c
alex <alexandreleblanc89@gmail.com>
2020-12-05 00:20:36 -0500

    update endpoint

--- Controllers/BuildingsOffController.cs
+++ Controllers/BuildingsOffController.cs
@@ -38,28 +38,6 @@ namespace RocketApi.Controllers
         }
 
 
-        // Retrieving a list of Buildings that contain at least one battery, column or elevator requiring intervention
-        // GET: api/BuildingsOff
-
-        // [HttpGet]
-        // public IEnumerable<Buildings> GetBuildings()
-        // {
-        //     IEnumerable<Buildings> Building =
-
-        //     from building in _context.Buildings
-        //     join battery in _context.Batteries on building.Id equals battery.BuildingId
-        //     join column in _context.Columns on battery.Id equals column.BatteryId
-        //     join elevator in _context.Elevators on column.Id equals elevator.ColumnId
-        //     where
-        //     battery.BatteryStatus == "Intervention" || battery.BatteryStatus == "Inactive"
-        //     || column.ColumnStatus == "Intervention" || column.ColumnStatus == "Inactive"
-        //     || elevator.ElevatorStatus == "Intervention" || elevator.ElevatorStatus == "Inactive"
-        //     select building;
-
-        //     return Building.Distinct().ToList();
-
-        // }
-
         private bool BuildingsExists(long id)
         {
             return _context.Buildings.Any(e => e.Id == id);
